2504d2817ee159a282bcb869cab1fec20511cec5
Cauã Bernardino <caua@jonajo.com>
2021-03-17 19:42:12 -0300

    PEP8 styling

--- newscompiler.py
+++ newscompiler.py
@@ -14,11 +14,21 @@ def main():
 
     # Check if parameters are alright
     try:
-        opts, args = getopt.getopt(argv, 'epsf', ['english', 'portuguese', 'spanish', 'french'])
+        opts, args = getopt.getopt(
+            argv, 
+            'epsf', 
+            [
+                'english', 
+                'portuguese', 
+                'spanish', 
+                'french'
+            ]
+        )
         
         if len(args) != 1 or len(opts) > 1:
             print("Usage: python newscompiler.py [language] 'SEARCH_TERM'")
-            print("Choose one language: [-e | --english] [-p | --portuguese] [-s | --spanish] [-f | --french]")
+            print("Choose one language: [-e | --english]" 
+                "[-p | --portuguese] [-s | --spanish] [-f | --french]")
             exit(1)
 
     except getopt.GetoptError:
@@ -46,7 +56,9 @@ def main():
     
     # Web scraping
     driver.get('https://news.google.com/')
-    searchBox = driver.find_element_by_xpath('//*[@id="gb"]/div[2]/div[2]/div/form/div[1]/div/div/div/div/div[1]/input[2]')
+    searchBox = driver.find_element_by_xpath(
+        '//*[@id="gb"]/div[2]/div[2]/div/form/div[1]/div/div/div/div/div[1]/input[2]'
+        )
     searchBox.send_keys(f"{args[0]} when:1d")
     searchBox.send_keys(Keys.RETURN)
 
@@ -54,14 +66,20 @@ def main():
     
     try:
         column = WebDriverWait(driver, 60).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/div[2]/div/main/c-wiz/div[1]'))
+            EC.presence_of_element_located((
+                By.XPATH, 
+                '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/div[2]/div/main/c-wiz/div[1]'
+                ))
         )
         pages = column.find_elements_by_class_name("DY5T1d")
     
         news = []
 
         for page in pages:
-            news.append((page.text, s.tinyurl.short(page.get_attribute('href'))))
+            news.append((
+                page.text, 
+                s.tinyurl.short(page.get_attribute('href'))
+                ))
 
         pdf_gen(args[0], news)
             
