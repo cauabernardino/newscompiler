187cdf9faa89b02069f48c2ba643eb59c9c22a20
Cauã Bernardino <cbernardino@live.com>
2021-09-10 19:38:01 -0300

    Path refactor and styling in pdf_gen.py

--- pdf_gen.py
+++ pdf_gen.py
@@ -3,7 +3,8 @@ from datetime import date
 import os
 
 
-fpdf.set_global("SYSTEM_TTFONTS", os.path.join(os.path.dirname(__file__),'fonts'))
+fpdf.set_global("SYSTEM_TTFONTS",
+                os.path.join(os.getcwd(), 'fonts'))
 
 
 def pdf_gen(search_term, lines):
@@ -12,21 +13,22 @@ def pdf_gen(search_term, lines):
     today_edit = date.today().strftime('%d/%B/%Y')
     term = search_term.upper()
     filename = " ".join((today, term))
-    
+
     # File creation
     pdf = FPDF(unit='mm', format='A4')
-    
+
     # Font adition
     pdf.add_font("Open Sans", style="", fname="OpenSans-Regular.ttf", uni=True)
     pdf.add_font("Open Sans", style="B", fname="OpenSans-Bold.ttf", uni=True)
     pdf.add_font("Open Sans", style="I", fname="OpenSans-Italic.ttf", uni=True)
-    pdf.add_font("Open Sans", style="BI", fname="OpenSans-BoldItalic.ttf", uni=True)
+    pdf.add_font("Open Sans", style="BI",
+                 fname="OpenSans-BoldItalic.ttf", uni=True)
 
     # Page creation
     pdf.add_page()
     pdf.set_font("Open Sans", 'B', size=12)
     pdf.cell(200, 10, txt=f"News for {term} on {today_edit}", ln=1, align="C")
-    
+
     # News addition
     count = 1
     pdf.set_font("Open Sans", '', size=10)
@@ -36,4 +38,4 @@ def pdf_gen(search_term, lines):
         count += 1
 
     # Output
-    pdf.output(".".join((filename,"pdf")), 'F')
+    pdf.output(".".join((filename, "pdf")), 'F')
