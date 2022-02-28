e7cb8745e3c4dbf8bb2d0cb8b228b19b48b17c9c
Cauã Bernardino <cbernardino@live.com>
2022-02-28 18:45:32 +0000

    Time Context implement.
        - New time context class to measure the elapsed time of news parsing
          and exporting
        - Refactoring of main

--- newscompiler/main.py
+++ newscompiler/main.py
@@ -1,34 +1,25 @@
-import requests
-import pyshorteners
-
-from time import time
-
-
 from newscompiler import SUPPORTED_LANGUAGES
+
 from newscompiler.news import get_news
 from newscompiler.parser import arg_parser
 from newscompiler.printer import export_pdf
+from newscompiler.tools import TimeContext
 
 
 def main():
 
-    # TODO Implement a time context manager
-    start = time()
-
     # Parsing arguments
     parser = arg_parser()
     args = parser.parse_args()
-    lang = SUPPORTED_LANGUAGES[args.lang]
 
-    # Getting the news
-    search_term = args.search_input
-    interval = args.time
-    news = get_news(search_term, interval, lang)
+    with TimeContext(True):
+        # Getting the news
+        lang = SUPPORTED_LANGUAGES[args.lang]
+        search_term = args.search_input
+        interval = args.time
 
-    export_pdf(search_term, news)
-
-    end = time()
-    print(f"Done in {round(end - start, 2)} seconds! :)")
+        news = get_news(search_term, interval, lang)
+        export_pdf(search_term, news)
 
 
 if __name__ == "__main__":
